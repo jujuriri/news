490d88249ffa24ec2d5d2841ac50d3a695103d40
Juri <sizumasa@hotmail.com>
2019-11-02 23:26:44 +0800

    Updates

---
+++ precache-manifest.49bf3c5741703ea6a3a70878d973fbec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0877873582f606f6576d80fbfb110f86",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "025287cfaf96c9350820",
+    "url": "/news/static/css/main.b1cfd7ad.chunk.css"
+  },
+  {
+    "revision": "1b454432ac1b7ad317f7",
+    "url": "/news/static/js/2.9cefb254.chunk.js"
+  },
+  {
+    "revision": "025287cfaf96c9350820",
+    "url": "/news/static/js/main.8a04cb55.chunk.js"
+  },
+  {
+    "revision": "d242938a1951cd935ec4",
+    "url": "/news/static/js/runtime-main.8ed446ba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/news/static/media/logo.25bf045c.svg"
+  }
+]);
